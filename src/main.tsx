a4719d08660595ab054f07a86e29dba2abf3ddf5
azprakash <1502prakash@gmail.com>
2026-06-07 20:56:49 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,23 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+// src/main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
 import App from './App.tsx';
 import './index.css';
+
+// Configure Cognito User Pool connection
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'ap-south-1_RGUJ3bzqY', // Replace with your User Pool ID
+      userPoolClientId: '758982998837', // Replace with your Client ID
+      loginWith: {
+        email: true,
+        phone: true,
+      }
+    }
+  }
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
